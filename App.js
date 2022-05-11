265a0be85c0fe49c56ce6aacce9f0a07a30ab726
ramona1402 <102229476+ramona1402@users.noreply.github.com>
2022-05-11 18:38:37 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import LoginPage from "./src/screens/LoginPage.js";
 import HomePage from "./src/screens/HomePage.js";
 import WorkoutsPage from "./src/screens/WorkoutsPage.js";
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+const Stack = createNativeStackNavigator ();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <WorkoutsPage></WorkoutsPage>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name ="Home" component={HomePage}/>
+          <Stack.Screen name ="Workouts" component={WorkoutsPage}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -22,3 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default App;
