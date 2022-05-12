448164011d1d552ab3cdb260e3497cb3d67dba8e
ramona1402 <102229476+ramona1402@users.noreply.github.com>
2022-05-12 11:54:43 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -5,19 +5,31 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginPage from "./src/screens/LoginPage.js";
 import HomePage from "./src/screens/HomePage.js";
 import WorkoutsPage from "./src/screens/WorkoutsPage.js";
+import RegisterPage from "./src/screens/RegisterPage.js";
+import CreateProfilePage from "./src/screens/CreateProfilePage.js";
 import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator ();
 
 const App = () => {
   return (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+    //view 1 pagina
+      /*<View style={styles.container}>
+        <LoginPage></ LoginPage>
+        <StatusBar style="auto" />
+      </View>*/
+
+      //view login + home + register + create profile + workouts
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Log In">
+          <Stack.Screen name ="Log In" component={LoginPage}/>
           <Stack.Screen name ="Home" component={HomePage}/>
+          <Stack.Screen name ="Register" component={RegisterPage}/>
+          <Stack.Screen name ="Create Profile" component={CreateProfilePage}/>
           <Stack.Screen name ="Workouts" component={WorkoutsPage}/>
         </Stack.Navigator>
     </NavigationContainer>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
